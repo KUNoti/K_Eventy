c000defbf052db1ac9ff7421b00fd970c2769dfb
WhatWR <kul.b@ku.th>
2024-03-16 14:43:56 +0700

    create navigation bottom

---
+++ lib/components/event_card.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:k_eventy/event.dart';
+
+class EventCard extends StatelessWidget {
+  final Event event;
+
+  const EventCard({super.key, required this.event});
+
+  void onTap() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        height: 300,
+        color: Colors.grey[300],
+        padding: const EdgeInsets.all(10),
+        child: Stack(
+          alignment:
+              Alignment.center, // Aligns children at the center of the stack
+          children: <Widget>[
+            // Bottom-most widget
+            Image.network(
+              event.image,
+              fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            // Middle widget
+            Positioned(
+              bottom: 20,
+              left: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    event.title,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Location: ${event.locationName}',
+                        style: const TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        'Creator: ${event.creator}',
+                        style: const TextStyle(
+                          fontSize: 10,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home/ar_setting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ARSetting extends StatefulWidget {
+  const ARSetting({super.key});
+
+  @override
+  State<ARSetting> createState() => _ARSettingState();
+}
+
+class _ARSettingState extends State<ARSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
