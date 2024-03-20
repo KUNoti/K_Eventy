62a407e614bf32385ed3ae0e7a3d75d5c6574321
WhatWR <kul.b@ku.th>
2024-03-20 12:33:22 +0700

    make map appear

--- lib/navigation/navigation_bottom.dart
+++ lib/navigation/navigation_bottom.dart
@@ -34,7 +34,28 @@ class _NavigationBottomState extends State<NavigationBottom> {
         width: 50,
         height: 50,
         child: FloatingActionButton(
-            onPressed: () {},
+            onPressed: () => showDialog(
+                context: context,
+        builder: (BuildContext context) => Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('This is a typical dialog.'),
+                const SizedBox(height: 15),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Close'),
+                ),
+              ],
+            ),
+          ),
+        ),
+            ),
             tooltip: 'Add New Item',
             elevation: 3,
             child: const Icon(Icons.add)),
--- lib/pages/home/map_section.dart
+++ lib/pages/home/map_section.dart
@@ -1,4 +1,9 @@
+
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapSection extends StatefulWidget {
   const MapSection({super.key});
@@ -8,10 +13,33 @@ class MapSection extends StatefulWidget {
 }
 
 class _MapSectionState extends State<MapSection> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(13.854529, 100.570012);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Map Section")),
+    return Scaffold(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 16.0,
+        ),
+          markers: {
+            const Marker(
+              markerId: MarkerId('Sydney'),
+              position: LatLng(13.8517, 100.5678),
+            )
+          },
+        gestureRecognizers: Set()
+          ..add(Factory<PanGestureRecognizer>(() => PanGestureRecognizer()))
+
+      ),
     );
   }
 }
